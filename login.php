b9e46036f69a0ce88c9213f3b28caf5415be3aac
Nanda777-gtg <nandawedanta@gmail.com>
2025-12-30 17:43:23 +0700

    memperbaiki tampilan login dan menambahkan logo user dan lock pada form username dan password

--- login.php
+++ login.php
@@ -11,7 +11,8 @@
     <title>Login</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-        <link rel="stylesheet" href="login.css">
+    <link rel="stylesheet" href="login.css">
+    <link href='https://cdn.boxicons.com/3.0.6/fonts/basic/boxicons.min.css' rel='stylesheet'>
 </head>
 <div class="d-flex justify-content-center align-items-center vh-100">
     <div class="box p-4 shadow bg-white rounded" sytle="width: 500px;">
@@ -21,12 +22,23 @@
             <form method="">
                 <div class="mb-4">
                     <label class="form-label">Username</label>
-                    <input type="text" class="form-control" name="username">
+                    <div class="input-group">
+                        <span class="input-group-text">
+                            <i class='bxr  bx-user'></i>
+                        </span>
+                        <input type="text" class="form-control ps-5" name="username">
+                    </div>
                     <div class="form-text">Silakan Masukan Username Anda</div>
                 </div>
+
                 <div class="mb-4">
                     <label class="form-label">Password</label>
-                    <input type="password" class="form-control" name="pass">
+                    <div class="input-group">
+                        <span class="input-group-text">
+                            <i class='bxr  bx-lock'></i>
+                        </span>
+                        <input type="password" class="form-control ps-5" name="pass">
+                    </div>
                 </div>
                 <button type="submit" class="btn btn-primary w-100">Login</button>
             </form>
