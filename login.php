a0f1f05fd019d74f610f8a5571992037c7d06f27
Nanda777-gtg <nandawedanta@gmail.com>
2025-12-30 16:30:51 +0700

    Membuat tampilan login web menggunakan html dan juga bootraps

---
+++ login.php
@@ -0,0 +1,37 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
+<div class="d-flex justify-content-center align-items-center vh-100">
+    <div class="box p-4 shadow bg-white rounded" sytle="width: 500px;">
+        <img src="" alt="">
+
+        <body class="body">
+            <form method="">
+                <div class="mb-4">
+                    <label class="form-label">Username</label>
+                    <input type="text" class="form-control" name="username">
+                    <div class="form-text">Silakan Masukan Username Anda</div>
+                </div>
+                <div class="mb-4">
+                    <label class="form-label">Password</label>
+                    <input type="password" class="form-control" name="pass">
+                </div>
+                <button type="submit" class="btn btn-primary w-100">Login</button>
+            </form>
+    </div>
+</div>
+
+</body>
+
+</html>
