c930845f4c65f8314304435901398a7c133d62d7
pratama <dityaloki@gmail.com>
2026-01-09 21:32:15 +0800

    memperbarui database

--- database/database.sql
+++ database/database.sql
@@ -1,111 +1,148 @@
-CREATE DATABASE  IF NOT EXISTS `e_voting` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `e_voting`;
--- MySQL dump 10.13  Distrib 8.0.44, for Win64 (x86_64)
+-- phpMyAdmin SQL Dump
+-- version 5.2.3
+-- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1    Database: e_voting
--- ------------------------------------------------------
--- Server version	8.0.44
+-- Host: localhost:3306
+-- Generation Time: Jan 09, 2026 at 01:31 PM
+-- Server version: 8.0.30
+-- PHP Version: 8.3.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `e_voting`
+--
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `anggota`
 --
 
-DROP TABLE IF EXISTS `anggota`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `anggota` (
-  `id_user` int NOT NULL AUTO_INCREMENT,
+  `id_user` int NOT NULL,
   `username` varchar(255) NOT NULL,
   `pass` varchar(255) NOT NULL,
-  PRIMARY KEY (`id_user`),
-  UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `role` enum('admin','user') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `anggota`
 --
 
-LOCK TABLES `anggota` WRITE;
-/*!40000 ALTER TABLE `anggota` DISABLE KEYS */;
-INSERT INTO `anggota` VALUES (1,'Nanda','wedanta123'),(2,'admin','admin123');
-/*!40000 ALTER TABLE `anggota` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `anggota` (`id_user`, `username`, `pass`, `role`) VALUES
+(24, 'admin', 'admin123', 'admin'),
+(25, 'user', 'user123', 'user');
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `tb_polling`
 --
 
-DROP TABLE IF EXISTS `tb_polling`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `tb_polling` (
-  `id_polling` int NOT NULL AUTO_INCREMENT,
+  `id_polling` int NOT NULL,
   `judul_polling` varchar(50) NOT NULL,
   `nama` varchar(50) NOT NULL,
   `Jkel` varchar(20) NOT NULL,
   `foto_calon` varchar(100) NOT NULL,
-  `jumlah_suara` int NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id_polling`)
-) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `jumlah_suara` int NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `tb_polling`
 --
 
-LOCK TABLES `tb_polling` WRITE;
-/*!40000 ALTER TABLE `tb_polling` DISABLE KEYS */;
-/*!40000 ALTER TABLE `tb_polling` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `tb_polling` (`id_polling`, `judul_polling`, `nama`, `Jkel`, `foto_calon`, `jumlah_suara`) VALUES
+(47, 'Pemilihan Ketua Umum Hima SI', 'Pratama', 'Laki-Laki', 'user.jpg', 1);
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `tb_vote`
 --
 
-DROP TABLE IF EXISTS `tb_vote`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `tb_vote` (
-  `id_vote` int NOT NULL AUTO_INCREMENT,
+  `id_vote` int NOT NULL,
   `id_user` int NOT NULL,
   `id_polling` int NOT NULL,
-  `waktu_vote` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id_vote`),
-  UNIQUE KEY `id_user` (`id_user`),
-  KEY `id_polling` (`id_polling`),
-  CONSTRAINT `tb_vote_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `anggota` (`id_user`),
-  CONSTRAINT `tb_vote_ibfk_2` FOREIGN KEY (`id_polling`) REFERENCES `tb_polling` (`id_polling`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `waktu_vote` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `tb_vote`
 --
 
-LOCK TABLES `tb_vote` WRITE;
-/*!40000 ALTER TABLE `tb_vote` DISABLE KEYS */;
-/*!40000 ALTER TABLE `tb_vote` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+INSERT INTO `tb_vote` (`id_vote`, `id_user`, `id_polling`, `waktu_vote`) VALUES
+(26, 25, 47, '2026-01-09 11:59:13');
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `anggota`
+--
+ALTER TABLE `anggota`
+  ADD PRIMARY KEY (`id_user`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- Indexes for table `tb_polling`
+--
+ALTER TABLE `tb_polling`
+  ADD PRIMARY KEY (`id_polling`);
+
+--
+-- Indexes for table `tb_vote`
+--
+ALTER TABLE `tb_vote`
+  ADD PRIMARY KEY (`id_vote`),
+  ADD UNIQUE KEY `id_user` (`id_user`),
+  ADD KEY `id_polling` (`id_polling`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `anggota`
+--
+ALTER TABLE `anggota`
+  MODIFY `id_user` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+
+--
+-- AUTO_INCREMENT for table `tb_polling`
+--
+ALTER TABLE `tb_polling`
+  MODIFY `id_polling` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
+
+--
+-- AUTO_INCREMENT for table `tb_vote`
+--
+ALTER TABLE `tb_vote`
+  MODIFY `id_vote` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `tb_vote`
+--
+ALTER TABLE `tb_vote`
+  ADD CONSTRAINT `tb_vote_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `anggota` (`id_user`),
+  ADD CONSTRAINT `tb_vote_ibfk_2` FOREIGN KEY (`id_polling`) REFERENCES `tb_polling` (`id_polling`);
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2026-01-05  9:16:49
